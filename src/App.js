fbd01b316aa51b6d42efbdcad8dcceafb6850f06
gabrielnafuzi <gabrielnafuzi@gmail.com>
2020-10-03 18:14:28 -0300

    created global styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyles from './styles/GlobalStyles';
+
 const App = () => {
-  return <div>Hello Ceres</div>;
+  return (
+    <>
+      <div>Hello Ceres</div>
+      <GlobalStyles />
+    </>
+  );
 };
 
 export default App;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  :root {
+    --color-background: #fff;
+    --color-black: #2e2e2e;
+    --color-purple: #7d77e0;
+    --color-white: #fff;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    min-height: 100%;
+    background: var(--color-background);
+  }
+
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: 'Space Mono', monospace;
+    color: var(--black);
+    transition: color 0.2s ease-out;
+  }
+
+  ul {
+    list-style: none;
+  }
+  
+`;
