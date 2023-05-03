f385f91f1ad7eaaef6ce2270745a350e4aa2a005
Gabriel Moraes <gabrielhm2001@gmail.com>
2023-05-03 18:39:18 -0300

    styles adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,75 +45,71 @@ const BlurBlob = ({ className }: { className?: string }) => {
 
 export default function Home() {
   return (
-    <main className="flex w-full flex-col overflow-hidden">
+    <main className="flex w-full flex-col overflow-hidden pb-6">
       <div className="mt-16 sm:mt-32 sm:px-8">
         <div className="mx-auto max-w-7xl lg:px-8">
-          <div className="relative px-4 sm:px-8 lg:px-12">
-            <div className="mx-auto max-w-2xl lg:max-w-5xl">
-              <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
-                <div className="lg:pl-20">
-                  <div className="relative mx-auto max-w-xs px-2.5 lg:max-w-none">
-                    <BlurBlob className="animation-delay-2000 -left-4 -top-4 bg-bluePurple" />
-                    <BlurBlob className="bottom-8 left-0 bg-lightBlue" />
-                    <BlurBlob className="animation-delay-4000 -bottom-5 -left-5 bg-darkBlue" />
+          <div className="relative mx-auto max-w-2xl px-4 sm:px-8 lg:max-w-5xl lg:px-12">
+            <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+              <div className="lg:pl-20">
+                <div className="relative mx-auto max-w-xs px-2.5 lg:max-w-none">
+                  <BlurBlob className="animation-delay-2000 -left-4 -top-4 bg-bluePurple" />
+                  <BlurBlob className="bottom-8 left-0 bg-lightBlue" />
+                  <BlurBlob className="animation-delay-4000 -bottom-5 -left-5 bg-darkBlue" />
 
-                    <BlurBlob className="animation-delay-4000 -right-4 -top-4 bg-deepRose" />
-                    <BlurBlob className="bottom-8 right-0 bg-coralRed" />
-                    <BlurBlob className="animation-delay-2000 -bottom-5 -right-5 bg-deepRose" />
+                  <BlurBlob className="animation-delay-4000 -right-4 -top-4 bg-deepRose" />
+                  <BlurBlob className="bottom-8 right-0 bg-coralRed" />
+                  <BlurBlob className="animation-delay-2000 -bottom-5 -right-5 bg-deepRose" />
 
-                    <div className="relative z-20">
-                      <Image
-                        src="/images/avatar.webp"
-                        alt="Gabriel Moraes"
-                        width={600}
-                        height={600}
-                        className="aspect-square rotate-3 rounded-2xl bg-zinc-800 object-cover"
-                      />
-                    </div>
+                  <div className="relative z-20">
+                    <Image
+                      src="/images/avatar.webp"
+                      alt="Gabriel Moraes"
+                      width={600}
+                      height={600}
+                      className="aspect-square rotate-3 rounded-2xl bg-zinc-800 object-cover"
+                    />
                   </div>
                 </div>
+              </div>
 
-                <div className="lg:order-first lg:row-span-2">
-                  <h1 className="whitespace-nowrap text-3xl font-bold leading-tight lg:text-6xl">
-                    Gabriel Moraes
-                  </h1>
+              <div className="lg:order-first lg:row-span-2">
+                <h1 className="whitespace-nowrap text-3xl font-bold leading-tight lg:text-6xl">
+                  Gabriel Moraes
+                </h1>
 
-                  <h2 className="mt-2 lg:mt-3 lg:text-2xl">
-                    Software Engineer
-                  </h2>
+                <h2 className="mt-2 lg:mt-3 lg:text-2xl">Software Engineer</h2>
 
-                  <div className="mt-4 leading-[1.8] text-zinc-400 md:max-w-xs lg:mt-9 lg:text-lg">
-                    <p>Learning something new every day.</p>
-                  </div>
+                <div className="mt-4 leading-[1.8] text-zinc-400 md:max-w-xs lg:mt-9 lg:text-lg">
+                  <p>Learning something new every day.</p>
                 </div>
+              </div>
 
-                <div className="lg:pl-20">
-                  <ul role="list">
-                    <SocialLink
-                      href="https://github.com/gabrielnafuzi"
-                      icon={Icons.Github}
-                      className="mt-4"
-                    >
-                      GitHub
-                    </SocialLink>
+              <div className="lg:pl-20">
+                <ul role="list">
+                  <SocialLink
+                    href="https://github.com/gabrielnafuzi"
+                    icon={Icons.Github}
+                    className="mt-4"
+                  >
+                    GitHub
+                  </SocialLink>
 
-                    <SocialLink
-                      href="https://linkedin.com/in/gabrielhenmoraes"
-                      icon={Icons.LinkedIn}
-                      className="mt-4"
-                    >
-                      LinkedIn
-                    </SocialLink>
+                  <SocialLink
+                    href="https://linkedin.com/in/gabrielhenmoraes"
+                    icon={Icons.LinkedIn}
+                    className="mt-4"
+                  >
+                    LinkedIn
+                  </SocialLink>
 
-                    <SocialLink
-                      href="mailto:gabriel.henmoraes@gmail.com"
-                      icon={Icons.Mail}
-                      className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
-                    >
-                      gabriel.henmoraes@gmail.com
-                    </SocialLink>
-                  </ul>
-                </div>
+                  <SocialLink
+                    href="mailto:gabriel.henmoraes@gmail.com"
+                    icon={Icons.Mail}
+                    className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+                  >
+                    gabriel.henmoraes@gmail.com
+                  </SocialLink>
+                </ul>
               </div>
             </div>
           </div>
